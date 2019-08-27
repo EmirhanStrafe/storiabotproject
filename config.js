36391b95c90ae5e3d1ee16c942aa5aa2c7f7879d
Glitch (storiabot) <none>
2019-08-27 08:48:56 +0000

    🐪🎽 Checkpoint ./config.js:6203935/108

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  "ownerID": "441169716547944448", //kendi IDınızı yazınız
-  "admins": ["441169716547944448"],
-  "support": ["441169716547944448"],
+  "ownerID": "499936631730470941", //kendi IDınızı yazınız
+  "admins": ["499936631730470941"],
+  "support": ["499936631730470941"],
   "token": "NjE1ODA1MTQ5MjAxMzAxNTA0.XWTqDQ.9sG81SH7QMTBBHRV65dZvyEGkSI", //botunuzun tokenini yazınız
   "dashboard" : {
     "oauthSecret": "9YZpwekNGLEm0pgpGneVHveQw33AjIR_", //botunuzun secretini yazınız 
